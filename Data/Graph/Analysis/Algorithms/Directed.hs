79c36d7f00741e13e5f7ac142610a0acb68f9291
Ivan.Miljenovic <Ivan.Miljenovic@gmail.com>
2008-09-29 22:44:05 +1000

    Fixed up and documented the Directed algorithms
    darcs-hash:20080929124405-97e08-d6b8db270ba6e0ddcc48783595e8b1a1ed6b5fc9

--- Data/Graph/Analysis/Algorithms/Directed.hs
+++ Data/Graph/Analysis/Algorithms/Directed.hs
@@ -1,33 +1,90 @@
-module Data.Graph.Analysis.Algorithms.Directed where
+{- |
+   Module      : Data.Graph.Analysis.Algorithms.Directed
+   Description : Algorithms for directed graphs.
+   Copyright   : (c) Ivan Lazar Miljenovic 2008
+   License     : 2-Clause BSD
+   Maintainer  : Ivan.Miljenovic@gmail.com
 
-import Data.Graph.Analysis.Graphalyze
+   Defines algorithms that work on both directed graphs.
+ -}
+module Data.Graph.Analysis.Algorithms.Directed
+    ( -- * Ending nodes
+      endNode, endNode',
+      endBy, endBy',
+      -- ** Root nodes
+      rootsOf, rootsOf',
+      isRoot, isRoot',
+      -- ** Leaf nodes
+      leavesOf, leavesOf',
+      isLeaf, isLeaf',
+      -- ** Singleton nodes
+      singletonsOf, singletonsOf',
+      isSingleton, isSingleton',
+      -- * Subgraphs
+      coreOf,
+    ) where
+
+import Data.Graph.Analysis.Types
+import Data.Graph.Analysis.Utils
 import Data.Graph.Analysis.Algorithms.Common
+
 import Data.Graph.Inductive.Graph
-import Data.Graph.Inductive.Tree
-import Data.Graph.Inductive.Basic
 import Data.List
-import Data.Maybe
-import Control.Arrow((***), first)
-import Control.Monad(join,liftM2)
-import qualified Data.Map as M
 
--- find those nodes where everything starts or stops
-    
-endNode        :: (AGr a -> Node -> [Node]) -> AGr a -> LNode a -> Bool
-endNode f g ln = case (f g n) of
-                   []   -> True
-                   [n'] -> n' == n
-                   _    -> False
-    where
-      n = node ln
+-- -----------------------------------------------------------------------------
+{- |
+   Find starting/ending nodes.
 
-rootsOf :: AGr a -> [LNode a]
-rootsOf = filterNodes isRoot
-    where
-      isRoot = endNode pre
+   We define an ending node as one where, given a function:
+   > f :: (Graph g) => g a b -> Node -> [Node]
+   the only allowed result is that node itself (to allow for loops).
+ -}
+
+-- | Determine if this 'LNode' is an ending node.
+endNode :: (Graph g) => (g a b -> Node -> NGroup) -> g a b -> LNode a -> Bool
+endNode f g ln = endNode' f g (node ln)
+
+-- | Determine if this 'Node' is an ending node.
+endNode'       :: (Graph g) => (g a b -> Node -> NGroup) -> g a b -> Node
+               -> Bool
+endNode' f g n = case (f g n) of
+                  []   -> True
+                  -- Allow loops
+                  [n'] -> n' == n
+                  _    -> False
+
+-- | Find all 'LNode's that meet the ending criteria.
+endBy   :: (Graph g) => (g a b -> Node -> NGroup) -> g a b -> LNGroup a
+endBy f = filterNodes (endNode f)
+
+-- | Find all 'Node's that match the ending criteria.
+endBy'   :: (Graph g) => (g a b -> Node -> NGroup) -> g a b -> NGroup
+endBy' f = filterNodes' (endNode' f)
+
+-- -----------------------------------------------------------------------------
+
+{- |
+   Root detection.
+ -}
+
+-- | Find all roots of the graph.
+rootsOf :: (Graph g) => g a b -> LNGroup a
+rootsOf = endBy pre
+
+-- | Find all roots of the graph.
+rootsOf' :: (Graph g) => g a b -> NGroup
+rootsOf' = endBy' pre
+
+-- | Returns @True@ if this 'LNode' is a root.
+isRoot :: (Graph g) => g a b -> LNode a -> Bool
+isRoot = endNode pre
+
+-- | Returns @True@ if this 'Node' is a root.
+isRoot' :: (Graph g) => g a b -> Node -> Bool
+isRoot' = endNode' pre
 
 {-
-classifyRoots    :: (Eq a) => GraphData a -> (Maybe (LNode a), [LNode a])
+classifyRoots    :: (Eq a) => GraphData a -> (Maybe (LNode a), LNGroup a)
 classifyRoots gr = first listToMaybe $ partition isWanted roots
     where
       roots = findRoots gr
@@ -38,30 +95,63 @@ wantedRootExists :: (Eq a) => GraphData a -> Bool
 wantedRootExists = isJust . fst . classifyRoots
 -}
 
---------------
 
-leavesOf :: AGr a -> [LNode a]
-leavesOf = filterNodes isLeaf
+-- -----------------------------------------------------------------------------
+
+{- |
+   Leaf detection.
+ -}
+
+-- | Find all leaves of the graph.
+leavesOf :: (Graph g) => g a b -> LNGroup a
+leavesOf = endBy pre
+
+-- | Find all leaves of the graph.
+leavesOf' :: (Graph g) => g a b -> NGroup
+leavesOf' = endBy' pre
+
+-- | Returns @True@ if this 'LNode' is a leaf.
+isLeaf :: (Graph g) => g a b -> LNode a -> Bool
+isLeaf = endNode pre
+
+-- | Returns @True@ if this 'Node' is a leaf.
+isLeaf' :: (Graph g) => g a b -> Node -> Bool
+isLeaf' = endNode' pre
+
+-- -----------------------------------------------------------------------------
+
+{- |
+   Singleton detection.
+ -}
+
+-- | Find all singletons of the graph.
+singletonsOf :: (Graph g) => g a b -> LNGroup a
+singletonsOf = endBy pre
+
+-- | Find all singletons of the graph.
+singletonsOf' :: (Graph g) => g a b -> NGroup
+singletonsOf' = endBy' pre
+
+-- | Returns @True@ if this 'LNode' is a singleton.
+isSingleton :: (Graph g) => g a b -> LNode a -> Bool
+isSingleton = endNode pre
+
+-- | Returns @True@ if this 'Node' is a singleton.
+isSingleton' :: (Graph g) => g a b -> Node -> Bool
+isSingleton' = endNode' pre
+
+-- -----------------------------------------------------------------------------
+
+{- |
+   The /core/ of the graph is the part of the graph containing all the
+   cycles, etc.  Depending on the context, it could be interpreted as
+   the part of the graph where all the "work" is done.
+ -}
+coreOf :: (DynGraph g, Eq a, Eq b) => g a b -> [g a b]
+coreOf = componentsOf . fixPointGraphs stripEnds
     where
-      isLeaf = endNode suc
-
-----
-
-singletonsOf :: AGr a -> [LNode a]
-singletonsOf = filterNodes isSingleton
-    where
-      isSingleton = endNode neighbors
-
-----
-
-coreOf :: (Eq a) => AGr a -> [AGr a]
-coreOf = componentsOf . fixPoint strip
-    where
-      strip gr' = removeNodes roots $ removeNodes leaves $ gr'
+      stripEnds gr' = delNodes roots $ delNodes leaves $ gr'
           where
-            roots = map node $ rootsOf gr'
-            leaves = map node $ leavesOf gr'
-            removeNode g = snd . flip match g
-            removeNodes ns g = foldl' removeNode g ns
-
-----
+            roots = rootsOf' gr'
+            leaves = leavesOf' gr'
+            delNodes ns g = foldl' (flip delNode) g ns
