f8ee74c4e4c3334367fda03edd5244c81549c2a7
Ivan.Miljenovic <Ivan.Miljenovic@gmail.com>
2008-09-29 22:42:57 +1000

    Wrappers around the Graphviz library
    darcs-hash:20080929124257-97e08-917a0e44b5ed5d9f1efee403a4f04dcfb0d32713

---
+++ Data/Graph/Analysis/Visualisation.hs
@@ -0,0 +1,38 @@
+{- |
+   Module      : Data.Graph.Analysis.Visualisation
+   Description : Graphviz wrapper functions
+   Copyright   : (c) Ivan Lazar Miljenovic 2008
+   License     : 2-Clause BSD
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   A wrapper module around the Haskell Graphviz library to turn Graphs
+   into basic graphs for processing by the Graphviz application.
+ -}
+module Data.Graph.Analysis.Visualisation where
+
+import Data.Graph.Analysis.Types
+import Data.Graph.Analysis.Utils
+import Data.Graph.Inductive.Graph
+import Data.GraphViz
+import Data.List
+
+-- | Turns the graph into DotGraph format with the given title.
+--   Nodes are labelled, edges aren't.
+graphviz     :: (Graph g, Show a, Ord b) => String -> g a b -> DotGraph
+graphviz t g = graphToDot g attrs nattrs eattrs
+    where
+      attrs = [Label t]
+      nattrs (_,a) = [Label (show a)]
+      eattrs _ = []
+
+-- | Turns the graph into DotGraph format with the given title.
+--   Cluster the nodes based upon their @ClusterLabel@ clusters.
+--   Nodes and clusters are labelled, edges aren't.
+graphvizClusters :: (Graph g, Show c, ClusterLabel a c, Ord b) =>
+                    String -> g a b -> DotGraph
+graphvizClusters t g = clusterGraphToDot g atts assignCluster catts natts eatts
+    where
+      atts = [Label t]
+      catts c = [Label (show c)]
+      natts (_,a) = [Label (nodelabel a)]
+      eatts _ = []
