f04b3515adb7ec5a152428c08ab6dcf84cd461f9
Ivan.Miljenovic <Ivan.Miljenovic@gmail.com>
2008-09-30 17:38:23 +1000

    Start on the Reporting framework
    darcs-hash:20080930073823-97e08-52079ef2578cd43ea9751366f5872a934dcb04fe

---
+++ Data/Graph/Analysis/Reporting.hs
@@ -0,0 +1,10 @@
+{- |
+   Module      : Data.Graph.Analysis.Reporting
+   Description : Graphalyze Types and Classes
+   Copyright   : (c) Ivan Lazar Miljenovic 2008
+   License     : 2-Clause BSD
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   This module defines the report framework used.
+ -}
+module Data.Graph.Analysis.Reporting
